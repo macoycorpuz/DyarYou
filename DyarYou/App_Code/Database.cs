3d16baa3b00a4f78be14f9350fac9593961ce210
Marcuz Corpuz <macoycorpuz06@gmail.com>
2017-06-25 12:50:51 +0800

    Initialization

---
+++ DyarYou/App_Code/Database.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Summary description for Database
+/// </summary>
+class Database : IDatabase, IDisposable
+{
+    #region Database Configurations
+
+    private string dataSource = "RYU-PC\\SQLEXPRESS";
+    private string initialCatalog = "db_GundamShop";
+    private string userID = "burger";
+    private string password = "burger";
+
+    //----Windows Authentication----
+    //Comment this line if NOT Windows Authentication
+    //private bool integratedSecurity = true;
+
+
+    //----SQL Authentication----
+    //Comment this line if NOT SQL Authentication 
+    private bool integratedSecurity = false;
+
+    #endregion
+
+    #region Attributes
+
+    private SqlConnection sqlCon = null;
+    private SqlConnectionStringBuilder sqlConString = null;
+    private SqlCommand sqlCmd;
+    private SqlDataAdapter sqlAdpt;
+    private DataTable data;
+    private bool hasError;
+
+    /// <summary>
+    /// Gets Datatable
+    /// </summary>
+    public DataTable Data
+    {
+        get { return data; }
+        private set { data = value; }
+    }
+
+    public bool HasError
+    {
+        get { return hasError; }
+        private set { hasError = value; }
+    }
+
+    #endregion
+
+    #region Constructors
+
+    public Database()
+    {
+        this.Process();
+    }
+
+    #endregion
+
+    #region Dispose
+
+    /// <summary>
+    /// Dispose all sql connection
+    /// </summary>
+    public void Dispose()
+    {
+        this.ConnectionClose();
+    }
+
+    #endregion
+
+    #region Process
+
+    public void Process()
+    {
+        this.ConnectionOpen();
+        this.ConnectionClose();
+    }
+
+    /// <summary>
+    /// Executes a Query
+    /// </summary>
+    /// <param name="command"></param>
+    public void ExecuteCommand(string query)
+    {
+        try
+        {
+            ConnectionOpen();
+            using (sqlCmd = new SqlCommand(query, sqlCon))
+            using (sqlAdpt = new SqlDataAdapter(sqlCmd))
+            {
+                data = new DataTable();
+                sqlAdpt.Fill(data);
+            }
+            ConnectionClose();
+            this.HasError = false;
+        }
+        catch (Exception ex)
+        {
+            this.HasError = true;
+            //MessageBox.Show("Unable to Execute Command!!\n\nError: " + ex.Message, "Database Error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+        }
+    }
+
+    /// <summary>
+    /// Executes Stored Procedures
+    /// </summary>
+    /// <param name="command"></param>
+    public void ExecuteStoredProc(string query, params object[] args)
+    {
+        try
+        {
+            ConnectionOpen();
+            using (sqlCmd = new SqlCommand(query, sqlCon))
+            using (sqlAdpt = new SqlDataAdapter(sqlCmd))
+            {
+                //Set Command Type to Stored Proc
+                sqlCmd.CommandType = CommandType.StoredProcedure;
+
+                //Construct Parameters
+                for (int i = 0; i < args.Length; i++)
+                {
+                    if (args[i] is string)
+                    {
+                        var sqlparam = new SqlParameter();
+                        sqlparam.ParameterName = (string)args[i];
+                        sqlparam.Value = args[++i];
+                        sqlCmd.Parameters.Add(sqlparam);
+                    }
+                    else if (args[i] is SqlParameter)
+                    {
+                        sqlCmd.Parameters.Add((SqlParameter)args[i]);
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Unknown sql parameter type");
+                    }
+                }
+
+                //Insert data
+                data = new DataTable();
+                sqlAdpt.Fill(data);
+            }
+            ConnectionClose();
+            this.HasError = false;
+        }
+        catch (Exception ex)
+        {
+            this.HasError = true;
+            //MessageBox.Show("Unable to Execute Stored Procedure!!\n\nError: " + ex.Message, "Database Error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+        }
+    }
+
+    #endregion
+
+    #region Connection
+
+    private void ConnectionOpen()
+    {
+        this.sqlCon = new SqlConnection();
+
+        try
+        {
+            this.sqlConString = new SqlConnectionStringBuilder();
+            this.sqlConString.DataSource = this.dataSource;
+            this.sqlConString.InitialCatalog = this.initialCatalog;
+            this.sqlConString.IntegratedSecurity = this.integratedSecurity;
+
+            if (this.integratedSecurity == false)
+            {
+                this.sqlConString.UserID = this.userID;
+                this.sqlConString.Password = this.password;
+            }
+
+            this.sqlCon.ConnectionString = this.sqlConString.ConnectionString;
+            this.sqlCon.Open();
+            this.HasError = false;
+        }
+        catch (Exception ex)
+        {
+            this.HasError = true;
+            //MessageBox.Show("Unable to connect database!!\n\nError: " + ex.Message, "Database Error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+        }
+    }
+
+    private void ConnectionClose()
+    {
+        if (this.sqlCon != null)
+        {
+            if (this.sqlCon.State == ConnectionState.Open)
+            {
+                this.sqlCon.Close();
+            }
+
+            this.sqlCon.Dispose();
+            this.sqlCon = null;
+        }
+    }
+
+    #endregion
+}
---
+++ DyarYou/App_Code/GlobalFunctions.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for GlobalFunctions
+/// </summary>
+public class GlobalFunctions
+{
+    #region Attributes
+
+    IDatabase _clsDb;
+    bool _Error;
+    //DataTable _dtProduct, _dtNews, _dtEvent, _dtCustomer, _dtAdmin, _dtTransaction;
+    //string _prodID, _newsID, eventID;
+
+    public bool HasError
+    {
+        get
+        {
+            return _Error;
+        }
+
+        set
+        {
+            _Error = value;
+        }
+    }
+
+    #endregion
+
+    #region Constructor
+
+    public GlobalFunctions()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// All View Processes
+    /// </summary>
+    #region Get
+
+
+    //EXAMPLE
+
+    //public DataTable GetProduct()
+    //{
+    //    _clsDb = new Database();
+    //    _dtProduct = new DataTable();
+
+    //    _clsDb.ExecuteCommand("Select ProductName, ProductID FROM Product ORDER BY ProductID DESC");
+    //    if (_clsDb.Data.Rows.Count > 0)
+    //        _dtProduct = _clsDb.Data;
+    //    return _dtProduct;
+
+    //}
+
+
+    #endregion
+
+    /// <summary>
+    /// All Add/Edit/Delete Processes
+    /// </summary>
+    #region Set
+
+    //EXAMPLE
+
+    //public void DeleteProduct(string title)
+    //{
+    //    _clsDb = new Database();
+    //    _clsDb.ExecuteStoredProc("spDeleteProduct", "@Title", title);
+    //}
+
+    //public void SetTransaction(string cid, string prodid, string qty)
+    //{
+    //    _clsDb = new Database();
+    //    _clsDb.ExecuteStoredProc("spAddTransaction", "@CID", cid, "@ProdID", prodid, "@OrderQty", qty);
+    //}
+
+    #endregion
+
+    #endregion
+}
