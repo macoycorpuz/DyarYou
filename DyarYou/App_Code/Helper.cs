6bc080d31a5cfb9146fdf0741ac0ba754186df88
Marcuz Corpuz <macoycorpuz06@gmail.com>
2017-06-25 20:11:58 +0800

    Nothing much

--- DyarYou/App_Code/Helper.cs
+++ DyarYou/App_Code/Helper.cs
@@ -9,288 +9,288 @@ using System.Configuration;
 /// Summary description for Helper
 /// </summary>
 
-    public class Helper : IDisposable
+public class Helper : IDisposable
+{
+    protected string _connString = null;
+    protected SqlConnection _conn = null;
+    protected SqlTransaction _trans = null;
+    protected bool _disposed = false;
+
+    /// <summary>
+    /// Sets or returns the connection string use by all instances of this class.
+    /// </summary>
+    public static string ConnectionString { get; set; }
+
+    /// <summary>
+    /// Returns the current SqlTransaction object or null if no transaction
+    /// is in effect.
+    /// </summary>
+    public SqlTransaction Transaction { get { return _trans; } }
+
+    /// <summary>
+    /// Constructor using global connection string.
+    /// </summary>
+    public Helper()
     {
-        protected string _connString = null;
-        protected SqlConnection _conn = null;
-        protected SqlTransaction _trans = null;
-        protected bool _disposed = false;
+        _connString = ConnectionString;
+        Connect();
+    }
 
-        /// <summary>
-        /// Sets or returns the connection string use by all instances of this class.
-        /// </summary>
-        public static string ConnectionString { get; set; }
+    /// <summary>
+    /// Constructure using connection string override
+    /// </summary>
+    /// <param name="connString">Connection string for this instance</param>
+    public Helper(string connString)
+    {
+        _connString = connString;
+        Connect();
+    }
 
-        /// <summary>
-        /// Returns the current SqlTransaction object or null if no transaction
-        /// is in effect.
-        /// </summary>
-        public SqlTransaction Transaction { get { return _trans; } }
+    // Creates a SqlConnection using the current connection string
+    protected void Connect()
+    {
+        _conn = new SqlConnection(_connString);
+        _conn.Open();
+    }
 
-        /// <summary>
-        /// Constructor using global connection string.
-        /// </summary>
-        public Helper()
+    /// <summary>
+    /// Constructs a SqlCommand with the given parameters. This method is normally called
+    /// from the other methods and not called directly. But here it is if you need access
+    /// to it.
+    /// </summary>
+    /// <param name="qry">SQL query or stored procedure name</param>
+    /// <param name="type">Type of SQL command</param>
+    /// <param name="args">Query arguments. Arguments should be in pairs where one is the
+    /// name of the parameter and the second is the value. The very last argument can
+    /// optionally be a SqlParameter object for specifying a custom argument type</param>
+    /// <returns></returns>
+    public SqlCommand CreateCommand(string qry, CommandType type, params object[] args)
+    {
+        SqlCommand cmd = new SqlCommand(qry, _conn);
+
+        // Associate with current transaction, if any
+        if (_trans != null)
+            cmd.Transaction = _trans;
+
+        // Set command type
+        cmd.CommandType = type;
+
+        // Construct SQL parameters
+        for (int i = 0; i < args.Length; i++)
         {
-            _connString = ConnectionString;
-            Connect();
-        }
-
-        /// <summary>
-        /// Constructure using connection string override
-        /// </summary>
-        /// <param name="connString">Connection string for this instance</param>
-        public Helper(string connString)
-        {
-            _connString = connString;
-            Connect();
-        }
-
-        // Creates a SqlConnection using the current connection string
-        protected void Connect()
-        {
-            _conn = new SqlConnection(_connString);
-            _conn.Open();
-        }
-
-        /// <summary>
-        /// Constructs a SqlCommand with the given parameters. This method is normally called
-        /// from the other methods and not called directly. But here it is if you need access
-        /// to it.
-        /// </summary>
-        /// <param name="qry">SQL query or stored procedure name</param>
-        /// <param name="type">Type of SQL command</param>
-        /// <param name="args">Query arguments. Arguments should be in pairs where one is the
-        /// name of the parameter and the second is the value. The very last argument can
-        /// optionally be a SqlParameter object for specifying a custom argument type</param>
-        /// <returns></returns>
-        public SqlCommand CreateCommand(string qry, CommandType type, params object[] args)
-        {
-            SqlCommand cmd = new SqlCommand(qry, _conn);
-
-            // Associate with current transaction, if any
-            if (_trans != null)
-                cmd.Transaction = _trans;
-
-            // Set command type
-            cmd.CommandType = type;
-
-            // Construct SQL parameters
-            for (int i = 0; i < args.Length; i++)
+            if (args[i] is string && i < (args.Length - 1))
             {
-                if (args[i] is string && i < (args.Length - 1))
-                {
-                    SqlParameter parm = new SqlParameter();
-                    parm.ParameterName = (string)args[i];
-                    parm.Value = args[++i];
-                    cmd.Parameters.Add(parm);
-                }
-                else if (args[i] is SqlParameter)
-                {
-                    cmd.Parameters.Add((SqlParameter)args[i]);
-                }
-                else throw new ArgumentException("Invalid number or type of arguments supplied");
+                SqlParameter parm = new SqlParameter();
+                parm.ParameterName = (string)args[i];
+                parm.Value = args[++i];
+                cmd.Parameters.Add(parm);
             }
-            return cmd;
-        }
-
-        #region Exec Members
-
-        /// <summary>
-        /// Executes a query that returns no results
-        /// </summary>
-        /// <param name="qry">Query text</param>
-        /// <param name="args">Any number of parameter name/value pairs and/or SQLParameter arguments</param>
-        /// <returns>The number of rows affected</returns>
-        public int ExecNonQuery(string qry, params object[] args)
-        {
-            using (SqlCommand cmd = CreateCommand(qry, CommandType.Text, args))
+            else if (args[i] is SqlParameter)
             {
-                return cmd.ExecuteNonQuery();
+                cmd.Parameters.Add((SqlParameter)args[i]);
             }
+            else throw new ArgumentException("Invalid number or type of arguments supplied");
         }
+        return cmd;
+    }
 
-        /// <summary>
-        /// Executes a stored procedure that returns no results
-        /// </summary>
-        /// <param name="proc">Name of stored proceduret</param>
-        /// <param name="args">Any number of parameter name/value pairs and/or SQLParameter arguments</param>
-        /// <returns>The number of rows affected</returns>
-        public int ExecNonQueryProc(string proc, params object[] args)
+    #region Exec Members
+
+    /// <summary>
+    /// Executes a query that returns no results
+    /// </summary>
+    /// <param name="qry">Query text</param>
+    /// <param name="args">Any number of parameter name/value pairs and/or SQLParameter arguments</param>
+    /// <returns>The number of rows affected</returns>
+    public int ExecNonQuery(string qry, params object[] args)
+    {
+        using (SqlCommand cmd = CreateCommand(qry, CommandType.Text, args))
         {
-            using (SqlCommand cmd = CreateCommand(proc, CommandType.StoredProcedure, args))
-            {
-                return cmd.ExecuteNonQuery();
-            }
+            return cmd.ExecuteNonQuery();
         }
+    }
 
-        /// <summary>
-        /// Executes a query that returns a single value
-        /// </summary>
-        /// <param name="qry">Query text</param>
-        /// <param name="args">Any number of parameter name/value pairs and/or SQLParameter arguments</param>
-        /// <returns>Value of first column and first row of the results</returns>
-        public object ExecScalar(string qry, params object[] args)
+    /// <summary>
+    /// Executes a stored procedure that returns no results
+    /// </summary>
+    /// <param name="proc">Name of stored proceduret</param>
+    /// <param name="args">Any number of parameter name/value pairs and/or SQLParameter arguments</param>
+    /// <returns>The number of rows affected</returns>
+    public int ExecNonQueryProc(string proc, params object[] args)
+    {
+        using (SqlCommand cmd = CreateCommand(proc, CommandType.StoredProcedure, args))
         {
-            using (SqlCommand cmd = CreateCommand(qry, CommandType.Text, args))
-            {
-                return cmd.ExecuteScalar();
-            }
+            return cmd.ExecuteNonQuery();
         }
+    }
 
-        /// <summary>
-        /// Executes a query that returns a single value
-        /// </summary>
-        /// <param name="proc">Name of stored proceduret</param>
-        /// <param name="args">Any number of parameter name/value pairs and/or SQLParameter arguments</param>
-        /// <returns>Value of first column and first row of the results</returns>
-        public object ExecScalarProc(string qry, params object[] args)
+    /// <summary>
+    /// Executes a query that returns a single value
+    /// </summary>
+    /// <param name="qry">Query text</param>
+    /// <param name="args">Any number of parameter name/value pairs and/or SQLParameter arguments</param>
+    /// <returns>Value of first column and first row of the results</returns>
+    public object ExecScalar(string qry, params object[] args)
+    {
+        using (SqlCommand cmd = CreateCommand(qry, CommandType.Text, args))
         {
-            using (SqlCommand cmd = CreateCommand(qry, CommandType.StoredProcedure, args))
-            {
-                return cmd.ExecuteScalar();
-            }
+            return cmd.ExecuteScalar();
         }
+    }
 
-
-        /// <summary>
-        /// Executes a query and returns the results as a SqlDataReader
-        /// </summary>
-        /// <param name="qry">Query text</param>
-        /// <param name="args">Any number of parameter name/value pairs and/or SQLParameter arguments</param>
-        /// <returns>Results as a SqlDataReader</returns>
-        public SqlDataReader ExecDataReader(string qry, params object[] args)
+    /// <summary>
+    /// Executes a query that returns a single value
+    /// </summary>
+    /// <param name="proc">Name of stored proceduret</param>
+    /// <param name="args">Any number of parameter name/value pairs and/or SQLParameter arguments</param>
+    /// <returns>Value of first column and first row of the results</returns>
+    public object ExecScalarProc(string qry, params object[] args)
+    {
+        using (SqlCommand cmd = CreateCommand(qry, CommandType.StoredProcedure, args))
         {
-            using (SqlCommand cmd = CreateCommand(qry, CommandType.Text, args))
-            {
-                return cmd.ExecuteReader();
-            }
+            return cmd.ExecuteScalar();
         }
+    }
 
-        /// <summary>
-        /// Executes a stored procedure and returns the results as a SqlDataReader
-        /// </summary>
-        /// <param name="proc">Name of stored proceduret</param>
-        /// <param name="args">Any number of parameter name/value pairs and/or SQLParameter arguments</param>
-        /// <returns>Results as a SqlDataReader</returns>
-        public SqlDataReader ExecDataReaderProc(string qry, params object[] args)
+
+    /// <summary>
+    /// Executes a query and returns the results as a SqlDataReader
+    /// </summary>
+    /// <param name="qry">Query text</param>
+    /// <param name="args">Any number of parameter name/value pairs and/or SQLParameter arguments</param>
+    /// <returns>Results as a SqlDataReader</returns>
+    public SqlDataReader ExecDataReader(string qry, params object[] args)
+    {
+        using (SqlCommand cmd = CreateCommand(qry, CommandType.Text, args))
         {
-            using (SqlCommand cmd = CreateCommand(qry, CommandType.StoredProcedure, args))
-            {
-                return cmd.ExecuteReader();
-            }
+            return cmd.ExecuteReader();
         }
+    }
 
-        /// <summary>
-        /// Executes a query and returns the results as a DataSet
-        /// </summary>
-        /// <param name="qry">Query text</param>
-        /// <param name="args">Any number of parameter name/value pairs and/or SQLParameter arguments</param>
-        /// <returns>Results as a DataSet</returns>
-        public DataSet ExecDataSet(string qry, params object[] args)
+    /// <summary>
+    /// Executes a stored procedure and returns the results as a SqlDataReader
+    /// </summary>
+    /// <param name="proc">Name of stored proceduret</param>
+    /// <param name="args">Any number of parameter name/value pairs and/or SQLParameter arguments</param>
+    /// <returns>Results as a SqlDataReader</returns>
+    public SqlDataReader ExecDataReaderProc(string qry, params object[] args)
+    {
+        using (SqlCommand cmd = CreateCommand(qry, CommandType.StoredProcedure, args))
         {
-            using (SqlCommand cmd = CreateCommand(qry, CommandType.Text, args))
-            {
-                SqlDataAdapter adapt = new SqlDataAdapter(cmd);
-                DataSet ds = new DataSet();
-                adapt.Fill(ds);
-                return ds;
-            }
+            return cmd.ExecuteReader();
         }
+    }
 
-        /// <summary>
-        /// Executes a stored procedure and returns the results as a Data Set
-        /// </summary>
-        /// <param name="proc">Name of stored proceduret</param>
-        /// <param name="args">Any number of parameter name/value pairs and/or SQLParameter arguments</param>
-        /// <returns>Results as a DataSet</returns>
-        public DataSet ExecDataSetProc(string qry, params object[] args)
+    /// <summary>
+    /// Executes a query and returns the results as a DataSet
+    /// </summary>
+    /// <param name="qry">Query text</param>
+    /// <param name="args">Any number of parameter name/value pairs and/or SQLParameter arguments</param>
+    /// <returns>Results as a DataSet</returns>
+    public DataSet ExecDataSet(string qry, params object[] args)
+    {
+        using (SqlCommand cmd = CreateCommand(qry, CommandType.Text, args))
         {
-            using (SqlCommand cmd = CreateCommand(qry, CommandType.StoredProcedure, args))
-            {
-                SqlDataAdapter adapt = new SqlDataAdapter(cmd);
-                DataSet ds = new DataSet();
-                adapt.Fill(ds);
-                return ds;
-            }
+            SqlDataAdapter adapt = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            adapt.Fill(ds);
+            return ds;
         }
-        public DataTable ExecDataTableProc(string qry, params object[] args)
+    }
+
+    /// <summary>
+    /// Executes a stored procedure and returns the results as a Data Set
+    /// </summary>
+    /// <param name="proc">Name of stored proceduret</param>
+    /// <param name="args">Any number of parameter name/value pairs and/or SQLParameter arguments</param>
+    /// <returns>Results as a DataSet</returns>
+    public DataSet ExecDataSetProc(string qry, params object[] args)
+    {
+        using (SqlCommand cmd = CreateCommand(qry, CommandType.StoredProcedure, args))
         {
-            using (SqlCommand cmd = CreateCommand(qry, CommandType.StoredProcedure, args))
-            {
-                SqlDataAdapter adapt = new SqlDataAdapter(cmd);
-                DataTable dt = new DataTable();
-                adapt.Fill(dt);
-                return dt;
-            }
+            SqlDataAdapter adapt = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            adapt.Fill(ds);
+            return ds;
         }
-        #endregion
-
-        #region Transaction Members
-
-        /// <summary>
-        /// Begins a transaction
-        /// </summary>
-        /// <returns>The new SqlTransaction object</returns>
-        public SqlTransaction BeginTransaction()
+    }
+    public DataTable ExecDataTableProc(string qry, params object[] args)
+    {
+        using (SqlCommand cmd = CreateCommand(qry, CommandType.StoredProcedure, args))
         {
-            Rollback();
-            _trans = _conn.BeginTransaction();
-            return Transaction;
+            SqlDataAdapter adapt = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapt.Fill(dt);
+            return dt;
         }
-
-        /// <summary>
-        /// Commits any transaction in effect.
-        /// </summary>
-        public void Commit()
-        {
-            if (_trans != null)
-            {
-                _trans.Commit();
-                _trans = null;
-            }
-        }
-
-        /// <summary>
-        /// Rolls back any transaction in effect.
-        /// </summary>
-        public void Rollback()
-        {
-            if (_trans != null)
-            {
-                _trans.Rollback();
-                _trans = null;
-            }
-        }
-
-        #endregion
-
-        #region IDisposable Members
-
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!_disposed)
-            {
-
-                if (disposing)
-                {
-                    if (_conn != null)
-                    {
-                        Rollback();
-                        _conn.Dispose();
-                        _conn = null;
-                    }
-                }
-                _disposed = true;
-            }
-        }
-
     }
     #endregion
+
+    #region Transaction Members
+
+    /// <summary>
+    /// Begins a transaction
+    /// </summary>
+    /// <returns>The new SqlTransaction object</returns>
+    public SqlTransaction BeginTransaction()
+    {
+        Rollback();
+        _trans = _conn.BeginTransaction();
+        return Transaction;
+    }
+
+    /// <summary>
+    /// Commits any transaction in effect.
+    /// </summary>
+    public void Commit()
+    {
+        if (_trans != null)
+        {
+            _trans.Commit();
+            _trans = null;
+        }
+    }
+
+    /// <summary>
+    /// Rolls back any transaction in effect.
+    /// </summary>
+    public void Rollback()
+    {
+        if (_trans != null)
+        {
+            _trans.Rollback();
+            _trans = null;
+        }
+    }
+
+    #endregion
+
+    #region IDisposable Members
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposed)
+        {
+
+            if (disposing)
+            {
+                if (_conn != null)
+                {
+                    Rollback();
+                    _conn.Dispose();
+                    _conn = null;
+                }
+            }
+            _disposed = true;
+        }
+    }
+
+}
+#endregion
 
